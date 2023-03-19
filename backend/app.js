6ed7985a031aaee42fb0da56080e82a279987f9c
aayush <sharma31aayush@gmail.com>
2023-03-19 21:12:16 +0530

    setup folders

--- backend/app.js
+++ backend/app.js
@@ -1,14 +1,48 @@
+// require("dotenv").config();
+// const express = require("express");
+// const app = express();
+
+// app.use(express.json());
+
+// app.get("/", (req, res) => {
+//   res.send("jobs api");
+// });
+
+// const port = process.env.PORT;
+
+// const start = async () => {
+//   try {
+//     app.listen(port, () =>
+//       console.log(`Server is listening on port ${port}...`)
+//     );
+//   } catch (error) {
+//     console.log(error);
+//   }
+// };
+
+// start();
+
 require("dotenv").config();
+require("express-async-errors");
 const express = require("express");
 const app = express();
 
-app.use(express.json());
+// error handler
+const notFoundMiddleware = require("./middleware/not-found");
+const errorHandlerMiddleware = require("./middleware/error-handler");
 
+app.use(express.json());
+// extra packages
+
+// routes
 app.get("/", (req, res) => {
   res.send("jobs api");
 });
 
-const port = process.env.PORT;
+app.use(notFoundMiddleware);
+app.use(errorHandlerMiddleware);
+
+const port = process.env.PORT || 3000;
 
 const start = async () => {
   try {
