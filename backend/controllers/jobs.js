13951545884040a501301d7d34584b779d86eaa8
aayush <sharma31aayush@gmail.com>
2023-03-20 22:38:29 +0530

    getALljobs && createJob feature

--- backend/controllers/jobs.js
+++ backend/controllers/jobs.js
@@ -1,13 +1,25 @@
+const Job = require("../models/Job");
+const { StatusCodes } = require("http-status-codes");
+const { BadRequestError, NotFoundError } = require("../errors");
+
+//only looking for jobs associated with a user
 const getAllJobs = async (req, res) => {
-  res.send("get all jobs");
+  const jobs = await Job.find({ createdBy: req.user.userId }).sort("createdAt");
+  res.status(StatusCodes.OK).json({ jobs, count: jobs.length });
 };
+
+//get single job
 const getJob = async (req, res) => {
   res.send("get single jobs");
 };
 
+//create job
 const createJob = async (req, res) => {
-  res.json(req.user);
+  req.body.createdBy = req.user.userId;
+  const job = await Job.create(req.body);
+  res.status(StatusCodes.CREATED).json({ job });
 };
+
 const updateJob = async (req, res) => {
   res.send("update job");
 };
