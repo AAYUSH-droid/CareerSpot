cf963ef7fa4bf5bd67bf5d82b85766fb522f1d78
aayush <sharma31aayush@gmail.com>
2023-03-21 19:38:08 +0530

    added / route

--- backend/app.js
+++ backend/app.js
@@ -36,6 +36,10 @@ app.use(cors());
 app.use(xss());
 app.use(rateLimiter());
 
+app.get("/", (req, res) => {
+  res.send('<h1>Jobs API</h1><a href="/api-docs">Documentation</a>');
+});
+
 // routes
 app.use("/api/v1/auth", authRouter);
 app.use("/api/v1/jobs", authenticateUser, jobsRouter);
